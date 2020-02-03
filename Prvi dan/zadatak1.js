129a3a0e6bbedc5248fa5f141b38f6f3b2ac6451
Milan <dobrijevic_milan@yahoo.com>
2020-02-03 23:07:35 +0100

    Prvi dan

---
+++ Prvi dan/zadatak1.js
@@ -0,0 +1,23 @@
+let matematicka_operacija = '%';
+let a = 20;
+let b = 9;
+switch(matematicka_operacija) {
+    case'+':
+    console.log(a + b);
+    break;
+    case'-':
+    console.log(a - b);
+    break;
+    case'*':
+    console.log(a * b);
+    break;
+    case'/':
+    console.log(a / b);
+    break;
+    case'%':
+    console.log(a % b);
+    break;
+    case'^':
+    console.log(a ^ b);
+    break;
+}
---
+++ Prvi dan/zadatak2.js
@@ -0,0 +1,9 @@
+let cost = 60;
+let euro = 200;
+if (cost - euro >= 0) {
+    console.log('Nemate dovoljno novca','Trenutno stanje','(ostaje isto)')
+}
+else
+{
+    console.log('Uspesno ste kupili proizvod','Trenutno stanje','(sada izmenjeno)')
+}
